bd318777287cd95c1947894a4cbbebbe2623e594
Dominika Włodarczyk <dominikaa.99@gmail.com>
2020-10-21 21:27:54 +0200

    Add modal and fixed side- and top- bars

--- src/js/script.js
+++ src/js/script.js
@@ -23,3 +23,52 @@ const options = {
 };
 
 flatpickr(calendarInput, options);
+
+
+// open modal
+
+function openModal(modal) {
+  document.querySelectorAll('#overlay > *').forEach( function(modal) {
+    modal.classList.remove('show');
+  });
+  document.querySelector('#overlay').classList.add('show');
+  document.querySelector(modal).classList.add('show');
+}
+
+// for quit option
+document.querySelector('.quit').addEventListener('click', function(e) {
+  e.preventDefault();
+  openModal('#quitModal');
+});
+
+// for chat
+document.querySelector('.manager-info').addEventListener('click', function(e) {
+  e.preventDefault();
+  openModal('#chatModal');
+});
+
+//close modal 
+
+function closeModal() {
+  document.getElementById('overlay').classList.remove('show');
+}
+
+document.querySelectorAll('#overlay .close-modal').forEach(function(btn) {
+  btn.addEventListener('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+});
+
+document.querySelector('#overlay').addEventListener('click', function(e) {
+  if(e.target === this) {
+    closeModal();
+  }
+});
+
+document.addEventListener('keyup', function(e) {
+  if(e.keyCode === 27) {
+    closeModal();
+  }
+});
+
